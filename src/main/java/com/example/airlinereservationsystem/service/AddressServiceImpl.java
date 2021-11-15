661074d5942c04113c025ac3448b50005d02bf70
Marat Muntinov <mmuntinov@miu.edu>
2021-11-15 13:35:59 -0500

    fix address
    fix address

--- src/main/java/com/example/airlinereservationsystem/service/AddressServiceImpl.java
+++ src/main/java/com/example/airlinereservationsystem/service/AddressServiceImpl.java
@@ -3,9 +3,14 @@ package com.example.airlinereservationsystem.service;
 import com.example.airlinereservationsystem.domain.Address;
 import com.example.airlinereservationsystem.repository.AddressRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -13,38 +18,9 @@ public class AddressServiceImpl implements AddressService {
     @Autowired
     private AddressRepository addressRepository;
 
-    @Override
-    public List<Address> getAllAddresses() {
-        return (List<Address>) addressRepository.findAll();
-    }
 
     @Override
-    public Address getAddress(long id) {
-        return addressRepository.findById(id).orElse(null);
-    }
-
-    @Override
-    public void deleteAddress(long id) {
-        addressRepository.deleteById(id);
-    }
-
-    private String street;
-    private String city;
-    private String zipCode;
-    private String state;
-
-    @Override
-    public Address updateAddress(Address updateAddress, long id) {
-        Address address = addressRepository.findById(id).orElse(null);
-        address.setStreet(updateAddress.getStreet());
-        address.setZipCode(updateAddress.getZipCode());
-        address.setCity(updateAddress.getCity());
-        address.setState(updateAddress.getState());
-        return addressRepository.save(address);
-    }
-
-    @Override
-    public void addAddress(Address address) {
-        addressRepository.save(address);
+    public Address getAddress(Long ID) {
+        return addressRepository.findById(ID).get();
     }
 }
