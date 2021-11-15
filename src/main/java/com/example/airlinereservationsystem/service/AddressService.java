ddc03df79fcc18a349176cb315952729ae80a4d5
Endagegnehu <endagegnehu@gmail.com>
2021-11-15 00:00:16 -0600

    add db config for actions

--- src/main/java/com/example/airlinereservationsystem/service/AddressService.java
+++ src/main/java/com/example/airlinereservationsystem/service/AddressService.java
@@ -2,6 +2,6 @@ package com.example.airlinereservationsystem.service;
 
 import com.example.airlinereservationsystem.domain.Address;
 
-public interface AddressService {
+public interface AddressService extends JpaRepository<Address,Long> {
     Address getAddresById(String ID);
 }
