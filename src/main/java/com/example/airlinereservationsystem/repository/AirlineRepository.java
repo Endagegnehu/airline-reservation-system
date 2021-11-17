34ac7c5d98820250a3ba9d5dafbffa35aac5591e
Marat Muntinov <mmuntinov@miu.edu>
2021-11-17 14:57:02 -0500

    add @Repository annotation

--- src/main/java/com/example/airlinereservationsystem/repository/AirlineRepository.java
+++ src/main/java/com/example/airlinereservationsystem/repository/AirlineRepository.java
@@ -5,8 +5,9 @@ import com.example.airlinereservationsystem.domain.Airport;
 import org.springframework.data.domain.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface AirlineRepository extends JpaRepository<Airline,Long>, PagingAndSortingRepository<Airline, Long> {
 
     Airline findByCode(String code);
--- src/main/java/com/example/airlinereservationsystem/repository/AirportRepository.java
+++ src/main/java/com/example/airlinereservationsystem/repository/AirportRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.domain.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface AirportRepository extends JpaRepository<Airport, Long>, PagingAndSortingRepository<Airport, Long> {
     Airport findByCode(String code);
 
